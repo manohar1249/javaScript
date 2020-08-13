0f1cd4519cfdc1568f4252d16f6dd60eaffd7678
manohar1249 <69627819+manohar1249@users.noreply.github.com>
2020-08-13 15:27:35 +0530

    Add files via upload

---
+++ readmultiline.js
@@ -0,0 +1,30 @@
+var readline = require('readline');
+
+var input = readline.createInterface({
+    input: process.stdin
+});
+
+const userinput = [];
+
+input.on('line',function(values){
+    userinput.push(values);
+})
+
+input.on('close',function(){
+    
+    console.log(sum(userinput));
+})
+
+
+
+
+function sum(data){
+    var num=0;
+    var arr = data;
+    for(var i=0;i<arr.length;i++){
+        arr[i]=parseInt(arr[i]);
+        
+        num=num+arr[i];
+    }
+    return num;
+}
